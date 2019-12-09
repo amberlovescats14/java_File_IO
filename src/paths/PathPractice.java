8f97c1da048da081f7972e5d5373e14f24895d4d
Amber Jones <amberlovescats14@gmail.com>
2019-12-09 09:10:01 -0600

    appended to the file

--- src/paths/PathPractice.java
+++ src/paths/PathPractice.java
@@ -1,34 +1,44 @@
 package paths;
 
 import java.io.IOException;
-import java.nio.channels.ScatteringByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 //z: Path is the core
 
 public class PathPractice {
-    public void checkPath(){
-        //z: folder, fileName
-        Path loremFile = Paths.get("files", "lorem.txt");
+    private List<String> animals = Arrays.asList("Shelby", "Punkin", "Sophia");
+    private Path loremFile = Paths.get("files", "lorem.txt");
 
-        //z: same unless you are using a relative path
-        loremFile.toString();
-        loremFile.getFileName();
-
-       boolean exists = Files.exists(loremFile);
+    public boolean checkPath(){
+        boolean exists = Files.exists(loremFile);
         System.out.println(exists);
+        return exists;
+    }
 
+    public void readLines(){
         try {
+            if(!checkPath()) throw new IOException();
             List<String> data = Files.readAllLines(loremFile);
             System.out.println(data);
         } catch(IOException ex) {
             System.out.printf("ERROR: %s\n", ex);
         }
-
-
-
     }
+
+    public void writeTo(){
+        try {
+            if(!checkPath()) throw new IOException();
+            Files.write(loremFile, animals, StandardOpenOption.APPEND);
+        } catch(IOException ex) {
+            System.out.printf("ERROR: %s\n", ex);
+        }
+    }
+
+
 }
--- src/paths/Test.java
+++ src/paths/Test.java
@@ -4,6 +4,8 @@ public class Test {
     public static void main(String[] args) {
         PathPractice pathsTest = new PathPractice();
         pathsTest.checkPath();
+        pathsTest.writeTo();
+        pathsTest.readLines();
     }
 
 
